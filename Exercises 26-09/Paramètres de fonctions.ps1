14936e33e046a7537a04c6da16235f66b51b85ea
Julian Andres Penso Rodriguez <6288022@cegepmontpetit.ca>
2025-09-26 10:29:12 -0400

    --- COMMIT EDIT MSG --- Add a script that calls the function with different parameters

--- Exercises 26-09/Paramètres de fonctions.ps1
+++ Exercises 26-09/Paramètres de fonctions.ps1
@@ -1,4 +1,4 @@
-﻿#Écrivez une fonction Start-Notepad qui démarre Notepad.exe à l'aide de Start-Process et retourne un objet décrivant le processus dans le pipeline.
+﻿#A) Écrivez une fonction Start-Notepad qui démarre Notepad.exe à l'aide de Start-Process et retourne un objet décrivant le processus dans le pipeline.
 function Start-Notepad {
     Start-Process Notepad.exe
     return Get-Process -name Notepad
@@ -18,9 +18,14 @@ Start-Notepad
 #C) Réécrivez la fonction précédente et faites en sorte qu'elle plante si le fichier n'existe pas. Utilisez les validations de paramètres.
 function Start-Notepad {
     param(
-        [Parameter(Mandatory=$true)] [string] $filePath = "C:\Users\6288022\Documents\GitHub\travaux-qui-comptent-pas\Exercises 26-09\salut!.txt"
+        [Parameter(Mandatory=$true)]
+        [ValidateScript({
+            if (-not (Test-Path $_)) {Write-Host "Le fichier '$_' n'existe pas"}
+            $true 
+        })]
+        [string] $FichierTexte
     )
-    Start-Process Notepad.exe -ArgumentList "C:\Users\6288022\Documents\GitHub\travaux-qui-comptent-pas\Exercises 26-09\salut!.txt"
+    Start-Process Notepad.exe -ArgumentList $FichierTexte
     return Get-Process -name Notepad
 }
 
