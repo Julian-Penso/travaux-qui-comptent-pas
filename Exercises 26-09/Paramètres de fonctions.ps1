fa4152ba5a5fb41e617df0415b5a39061e210a3c
Julian Andres Penso Rodriguez <6288022@cegepmontpetit.ca>
2025-09-26 09:43:09 -0400

    commit 1 (HEAD -> main, origin/main, origin/HEAD) Author: 6288022 < Date:   Wed Sep 27 09:15:00 2023 +0200
        Correction exercice Invoke-Addition
    diff --git a/Exercises 26-09/Invoke-Addition.PS1 b/Exercises 26-09/Invoke-Addition.PS1

---
+++ Exercises 26-09/Paramètres de fonctions.ps1
@@ -0,0 +1,7 @@
+﻿#Écrivez une fonction Start-Notepad qui démarre Notepad.exe à l'aide de Start-Process et retourne un objet décrivant le processus dans le pipeline.
+function Start-Notepad {
+    Start-Process Notepad.exe
+    return Get-Process -name Notepad
+}
+
+Start-Notepad
